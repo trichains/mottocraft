b647bb91f6f004b111c0b43b6ebe5f33fb7d4921
Cristhian Almeida <crilordz@hotmail.com>
2023-09-21 18:58:48 -0400

    add: galeria com js

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imgs img');
+const galeriaContainer = document.querySelector('.bicicleta-imgs');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 1000px)').matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
