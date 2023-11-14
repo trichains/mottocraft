ddb224b11b7a59f9a36d31f85ea63c7f41d26aa4
Cristhian Almeida <3correntes@gmail.com>
2023-11-14 18:48:28 -0400

    add: horario de funcionamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
+// Ativar horário de funcionamento
+function initFuncionamento() {
+  const funcionamento = document.querySelector('[data-semana]');
+  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+  const data = new Date();
+  const diaAgora = data.getDay();
+  const horarioAgora = data.getHours();
+
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+
+  const horarioAberto =
+    horarioSemana[0] <= horarioAgora && horarioSemana[1] > horarioAgora;
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add('aberto');
+  }
+}
+initFuncionamento();
+
 // Ativar links do menu
-const links = document.querySelectorAll(".header-menu a");
+const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
 
   if (url.includes(href)) {
-    link.classList.add("ativo");
+    link.classList.add('ativo');
   }
 }
 
@@ -27,39 +46,39 @@ parametros.forEach(ativarProduto);
 
 // Perguntas Frequentes
 
-const perguntas = document.querySelectorAll(".perguntas button");
+const perguntas = document.querySelectorAll('.perguntas button');
 
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute("aria-controls");
+  const controls = pergunta.getAttribute('aria-controls');
   const resposta = document.getElementById(controls);
 
-  resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains("ativa");
-  pergunta.setAttribute("aria-expanded", ativa);
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
 }
 
 function eventosPerguntas(pergunta) {
-  pergunta.addEventListener("click", ativarPergunta);
+  pergunta.addEventListener('click', ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
 
 // Galeria de motocicletas
 
-const galeria = document.querySelectorAll(".moto-imgs img");
-const galeriaContainer = document.querySelector(".moto-imgs");
+const galeria = document.querySelectorAll('.moto-imgs img');
+const galeriaContainer = document.querySelector('.moto-imgs');
 
 function trocarImagem(event) {
   const img = event.currentTarget;
-  const media = matchMedia("(min-width: 1000px)").matches;
+  const media = matchMedia('(min-width: 1000px)').matches;
   if (media) {
     galeriaContainer.prepend(img);
   }
 }
 
 function eventosGaleria(img) {
-  img.addEventListener("click", trocarImagem);
+  img.addEventListener('click', trocarImagem);
 }
 
 galeria.forEach(eventosGaleria);
